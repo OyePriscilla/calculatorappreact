e2aa3a6e9e3daed291350259ce162e71f5540982
Oyebade Priscilla <oyebadepriscilla22@gmail.com>
2022-08-09 16:17:36 +0100

    Add routes to the home, calculator and quotes pages

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Quote from './pages/Quote';
+import Calculator from './component/calculator';
+import Header from './layout/Header';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="calculator" element={<Calculator />} />
+        <Route path="quote" element={<Quote />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 );
